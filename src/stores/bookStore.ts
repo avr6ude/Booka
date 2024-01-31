dfa75fda7d145e34edfbd4d378c123c8cbf7f897
avr6ude <canuckchan@gmail.com>
2024-01-31 13:37:22 +0100

    removed default values for book atom

--- src/stores/bookStore.ts
+++ src/stores/bookStore.ts
@@ -1,19 +1,3 @@
 import { atom } from "jotai";
 
-export const bookStore = atom([
-  {
-    id: 1,
-    title: "The Lord of the Rings",
-    author: "J.R.R. Tolkien"
-  }, 
-  {
-    id: 2,
-    title: "Harry Potter",
-    author: "J.K. Rowling"
-  }, 
-  {
-    id: 3,
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien"
-  }
-])
+export const bookStore = atom<Book[]>([])
