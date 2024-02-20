95f04e61c24ecab452069ac655a62ed1805d53d2
avr6ude <canuckchan@gmail.com>
2024-02-20 12:46:13 +0100

    add preset for watermelondb

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo', 'module: metro-react-native-babel-preset'],
+    presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
       ['@babel/plugin-proposal-decorators', { legacy: true }],
       ['@babel/plugin-proposal-class-properties', { loose: true }],
       ['@babel/plugin-transform-runtime', { helpers: true, regenerator: true }],
+      'react-native-reanimated/plugin',
     ],
   }
 }
