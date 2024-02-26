dc78c3afb964078c7a5d47510ab3fea2a1c68765
avr6ude <canuckchan@gmail.com>
2024-02-26 17:35:15 +0100

    fix: theme switching

--- components/AppProvider.tsx
+++ components/AppProvider.tsx
@@ -2,18 +2,31 @@ import { database } from '@/app/_layout'
 import { theme, themeLight } from '@/constants/themes'
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
 import { DatabaseProvider } from '@nozbe/watermelondb/DatabaseProvider'
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+  useTheme,
+} from '@react-navigation/native'
 import { DripsyProvider } from 'dripsy'
 import { useColorScheme } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export default function AppProvider({ children }: any) {
   const colorMode = useColorScheme()
+  const themeSelector = colorMode === 'dark' ? theme : themeLight
+  const navigationThemeSelector =
+    colorMode === 'dark' ? DarkTheme : DefaultTheme
 
   return (
-    <DripsyProvider theme={colorMode === 'dark' ? theme : themeLight}>
+    <DripsyProvider theme={themeSelector}>
       <DatabaseProvider database={database}>
         <GestureHandlerRootView style={{ flex: 1 }}>
-          <BottomSheetModalProvider>{children}</BottomSheetModalProvider>
+          <BottomSheetModalProvider>
+            <ThemeProvider value={navigationThemeSelector}>
+              {children}
+            </ThemeProvider>
+          </BottomSheetModalProvider>
         </GestureHandlerRootView>
       </DatabaseProvider>
     </DripsyProvider>
